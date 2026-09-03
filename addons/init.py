9b9f32110a153ec40da25b4935fd700a6d8da993
samzthedesigner-hue <samzthedesigner@gmail.com>
2026-09-03 20:12:49 +0100

    Create init.py for add-ons module

---
+++ addons/init.py
@@ -0,0 +1,7 @@
+"""Add-ons module for AEGIS Knowledge Engine"""
+
+from addons.manager import AddonManager
+from addons.api import AddonAPI
+from addons.repository import AddonRepository
+
+__all__ = ['AddonManager', 'AddonAPI', 'AddonRepository']
